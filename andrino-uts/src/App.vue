720b57efc235e97d3c30a1c9a28f46e2875b7303
DelaAzahra <andrinosyaddani5@gmail.com>
2025-04-26 13:04:29 +0700

    Menginisialisasi variable

---
+++ andrino-uts/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+
+const tasks = ref([
+  { name: "Belajar Vue", done: false },
+  { name: "Mengerjakan tugas", done: true },
+  { name: "Olahraga pagi", done: false },
+  { name: "Membaca buku", done: true }
+]);
+
+const newTask = ref("");
+const inputField = ref(null);
+
+const isEditing = ref(false);
+const editIndex = ref(null);
+const showCompleted = ref(true);
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
