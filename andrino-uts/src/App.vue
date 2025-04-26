1e30f228713fac48b5865b1fc42a99bdb2654830
DelaAzahra <andrinosyaddani5@gmail.com>
2025-04-26 13:11:58 +0700

    Untuk mengfilter kegiatan

--- andrino-uts/src/App.vue
+++ andrino-uts/src/App.vue
@@ -23,6 +23,41 @@ const addTask = () => {
   }
 };
 
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+  cancelEdit();
+};
+
+const editTask = (index) => {
+  newTask.value = tasks.value[index].name;
+  isEditing.value = true;
+  editIndex.value = index;
+  inputField.value.focus();
+};
+
+const updateTask = () => {
+  if (editIndex.value !== null && newTask.value.trim() !== "") {
+    tasks.value[editIndex.value].name = newTask.value.trim();
+    cancelEdit();
+  }
+};
+
+const cancelEdit = () => {
+  newTask.value = "";
+  isEditing.value = false;
+  editIndex.value = null;
+};
+
+const filteredTasks = computed(() => {
+  return showCompleted.value
+    ? tasks.value
+    : tasks.value.filter(task => !task.done);
+});
+
+onMounted(() => {
+  inputField.value?.focus();
+});
+
 </script>
 
 <template>
